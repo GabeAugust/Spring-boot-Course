4980b90614528d6b0a29dd722f9db33737070edd
Gabriel Pereira <gatop652@gmail.com>
2025-11-13 13:28:09 -0300

    feat: Controller Class

--- src/main/java/dev/java10x/cadastrodeninjas/CadastroDeNinjasApplication.java
+++ src/main/java/dev/java10x/cadastrodeninjas/CadastroDeNinjasApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CadastrodeninjasApplication {
+public class CadastroDeNinjasApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CadastrodeninjasApplication.class, args);
+		SpringApplication.run(CadastroDeNinjasApplication.class, args);
 	}
+
 
 }
