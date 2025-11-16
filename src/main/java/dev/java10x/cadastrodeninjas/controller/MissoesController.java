75d5203c84ada633d0f0a54d1b0920fce4c695c8
Gabriel Pereira <gatop652@gmail.com>
2025-11-16 20:27:06 -0300

    feat: notations controller in missoes controller

--- src/main/java/dev/java10x/cadastrodeninjas/controller/MissoesController.java
+++ src/main/java/dev/java10x/cadastrodeninjas/controller/MissoesController.java
@@ -1,4 +1,9 @@
 package dev.java10x.cadastrodeninjas.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class MissoesController {
 }
