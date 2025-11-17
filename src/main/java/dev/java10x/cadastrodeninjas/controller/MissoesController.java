eeed6245718c61a4d48f0fe1adc55a2a4efd0c62
Gabriel Pereira <gatop652@gmail.com>
2025-11-17 20:18:38 -0300

    feat: endpoint /missoes

--- src/main/java/dev/java10x/cadastrodeninjas/controller/MissoesController.java
+++ src/main/java/dev/java10x/cadastrodeninjas/controller/MissoesController.java
@@ -1,9 +1,33 @@
 package dev.java10x.cadastrodeninjas.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
 public class MissoesController {
+
+    @PostMapping("/missoes")
+    public String addMission(){
+        return "aaaa";
+    }
+
+    @GetMapping("/missoes")
+    public String listMissions(){
+        return "aaaa";
+    }
+
+    @GetMapping("/missoes/{id}")
+    public String listMission(){
+        return "";
+    }
+
+    @PutMapping("/missoes/{id}")
+    public String editMission(){
+        return "";
+    }
+
+    @DeleteMapping("/missoes/{id}")
+    public String deleteMission(){
+        return "";
+    }
 }
--- src/main/java/dev/java10x/cadastrodeninjas/model/MissoesModel.java
+++ src/main/java/dev/java10x/cadastrodeninjas/model/MissoesModel.java
@@ -27,4 +27,7 @@ public class MissoesModel {
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninja;
 
+
+
+
 }
